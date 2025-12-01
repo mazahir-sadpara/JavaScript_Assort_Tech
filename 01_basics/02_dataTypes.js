d4debcbf9f7aa2b27694e4f2ccabe8b58de18d08
mazahir-sadpara <hmazahir45@gmail.com>
2025-12-01 18:08:38 +0500

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all code as newer version
+
+// alert(3 + 3) //we are using Node.js not Browser
+
+console.log(3 + 
+    
+    3); //code readability should be high
+console.log("Hello")
+
+let name = "Mazahir"
+let age = 25
+let isLoggedIn = true
+
+//number => 2 power 53
+//bigint 
+//string => ""
+//boolean => true / false
+//null=>standalone value hai
+//undefined
+//symbol => unique //const id = Symbol("id");
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
