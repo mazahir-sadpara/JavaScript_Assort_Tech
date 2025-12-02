bed8ada5a94cb7853ef497dfcd96dcb138dc0f64
mazahir-sadpara <hmazahir45@gmail.com>
2025-12-02 17:31:18 +0500

    Numbers and Maths

---
+++ 01_basics/07_numbersAndMaths.js
@@ -0,0 +1,41 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(30000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 56.899755
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//************************ Maths **********************************
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(9.4))
+// console.log(Math.ceil(9.1))
+// console.log(Math.floor(9.9))
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(3, 3));
+// console.log(Math.min(6, 700, 7, 3, -6));
+// console.log(Math.max(6, 700, 7, 3, -6));
+// console.log(Math.PI);
+
+console.log((Math.random()*10) +1);
+console.log(Math.floor((Math.random()*10) +1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
