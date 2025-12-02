c7d284b7172ef8a23444ede9d880d29dbe78f193
mazahir-sadpara <hmazahir45@gmail.com>
2025-12-02 12:16:41 +0500

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,12 +26,61 @@
 // ""=> false 
 // "Mazahir"=> true
 
-let someNumber = 45;
+// let someNumber = 45;
 
-let stringValue = String(someNumber)
+// let stringValue = String(someNumber)
 
-console.log(typeof stringValue);
-console.log(stringValue);
+// console.log(typeof stringValue);
+// console.log(stringValue);
+
+
+// ********************Operations***************************
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+
+str1 = "Hello"
+str2 = " Mazahir"
+
+str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("2" + 2);
+// console.log(2 + "2");
+// console.log("2" + 2 +2);
+// console.log(2 + 2 + "2");
+
+// console.log((2+2)*5%4);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.log(num1);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
 
 
